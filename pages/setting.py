f4b82ba640285c4ecb2f68fbb423adc374fdb829
Vitaly068 <vsvolkow@gmail.com>
2023-10-28 14:50:36 +0300

    Initial commit

---
+++ pages/setting.py
@@ -0,0 +1,10 @@
+valid_email = 'ookami068@gmail.com'
+valid_phone = '+7(915)8721554'
+valid_password = 'Ss123456789'
+valid_login = 'ookami'
+valid_ls = '1698337741823'
+valid_username = u"Окамов"
+ad_slogan = u"Персональный помощник в цифровом мире Ростелекома"
+site = 'https://b2c.passport.rt.ru'
+link_lk = "/account_b2c/page"
+drv_path = 'С:/Users/Виталий/PycharmProjects/Driver/chromedriver.exe '
