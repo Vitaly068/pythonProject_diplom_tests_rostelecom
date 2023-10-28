03bd5dd41086c3abc53f71d20c4834517e255175
Vitaly068 <135343022+Vitaly068@users.noreply.github.com>
2023-10-28 15:02:15 +0300

    setting.py

--- pages/setting.py
+++ pages/setting.py
@@ -1,10 +1,10 @@
-valid_email = 'ookami068@gmail.com'
-valid_phone = '+7(915)8721554'
-valid_password = 'Ss123456789'
-valid_login = 'ookami'
-valid_ls = '1698337741823'
+valid_email = '********@gmail.com' #валидные данные скрыты
+valid_phone = '+7(9***)8****54' #валидные данные скрыты
+valid_password = '**123456789' #валидные данные скрыты
+valid_login = '***ami' #валидные данные скрыты
+valid_ls = '***8337741***' #валидные данные скрыты
 valid_username = u"Окамов"
 ad_slogan = u"Персональный помощник в цифровом мире Ростелекома"
 site = 'https://b2c.passport.rt.ru'
 link_lk = "/account_b2c/page"
-drv_path = 'С:/Users/Виталий/PycharmProjects/Driver/chromedriver.exe '
+drv_path = 'С:***//PycharmProjects/Driver/chromedriver.exe '
